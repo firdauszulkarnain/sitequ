979599c8b4c7a75ae9d5a22ab01581241a666fcd
firdauszulkarnain <muhammadfirdauszulkarnain03@gmail.com>
2022-05-31 20:10:37 +0800

    detail blade

--- app/Http/Controllers/SurahController.php
+++ app/Http/Controllers/SurahController.php
@@ -14,10 +14,15 @@ class SurahController extends Controller
         quran:$name quran:TermasukGolonganSurah ?golongan .
         quran:$name quran:MengandungArtiSurah ?arti .
         }";
+        $queryAyat = "SELECT * WHERE {quran:$name quran:MengandungAyat ?ayat.}";
         $resultDetail = $this->sparql->query($query);
+        $ayat = $this->sparql->query($queryAyat);
         $dataGolongan = [];
         $dataTema = [];
         $dataArtiSurah = [];
+        $dataAyat = [];
+        $dataKalimat = [];
+        $kalimat = '';
 
         foreach ($resultDetail as $row) {
 
@@ -49,11 +54,35 @@ class SurahController extends Controller
             }
         }
 
+        foreach ($ayat as $row) {
+            $dataKalimat = preg_split("/[\s,]+/", $this->result($row->ayat->getValue()));
+            $j = 2;
+            $i = 1;
+            foreach ($dataKalimat as $kata) {
+                $nomor = $i . ".";
+                if ($kata == $nomor) {
+                    unset($kata);
+                    $kalimat = '';
+                } else {
+                    $kalimat = $kalimat . ' ' . $kata;
+                    $nomorAkhir =  $j . ".";
+                    if ($kata == $nomorAkhir) {
+                        $kalimat = preg_replace("/$nomorAkhir/", "", $kalimat);
+                        $dataAyat[] = $kalimat;
+                        $kalimat = '';
+                        $j++;
+                    }
+                }
+                $i++;
+            }
+        }
+
         $data = [
             'title' => 'Detail Surah ' . $name,
             'tema' => $dataTema,
             'golongan' => $dataGolongan,
             'arti' => $dataArtiSurah,
+            'ayat' => $dataAyat
         ];
 
         return view('detail', $data);
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -11,8 +11,8 @@
         </section>
 
         <section class="content">
-            <div class="row d-flex justify-content-center">
-                <div class="col-lg-8">
+            <div class="row">
+                <div class="col-lg-4">
                     <div class="card">
                         <div class="card-body">
                             <p class="font-weight-bolder"> <i class="fas fa-map-pin"></i> Tema Surah</p>
@@ -28,6 +28,35 @@
                                     <li><a href="">{{ $item['arti'] }}</a></li>
                                 @endforeach
                             </ul>
+                            <hr>
+                            <p class="font-weight-bolder"> <i class="fas fa-map-pin"></i> Golongan Surah</p>
+                            <ul class="mt-n2">
+                                @foreach ($golongan as $item)
+                                    <li><a href="">{{ $item['golongan'] }}</a></li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="card">
+                        <div class="card-header bg-light font-weight-bolder text-center">
+                            MAKNA AYAT
+                        </div>
+                        <div class="card-body px-4 py-4">
+                            @php
+                                $i = 1;
+                            @endphp
+                            @foreach ($ayat as $item)
+                                <span class="badge badge-light"></span>
+                                <p class="mt-n2 text-justify font-weight-bolder bg-light p-3 border border-secondary shadow-sm"
+                                    style="font-size: 13px !important; line-height: 1.3;">
+                                    {{ $i }}.{{ $item }}
+                                </p>
+                                @php
+                                    $i++;
+                                @endphp
+                            @endforeach
                         </div>
                     </div>
                 </div>
